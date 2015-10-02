3cd9816df036e3c0bd597fee7c242826814fd150
Arianna Aondio <arianna.aondio@varnish-software.com>
2015-10-02 11:15:01 +0200

    Porting to VMOD to V4.1

--- src/vmod_example.c
+++ src/vmod_example.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "vcl.h"
 #include "vrt.h"
 #include "cache/cache.h"
 
 #include "vcc_if.h"
 
 int
-init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
+init_function(const struct vrt_ctx *ctx, struct vmod_priv *priv, enum vcl_event_e e)
 {
+	if (e != VCL_EVENT_LOAD)
+		return (0);
+
+	/* init what you need */
 	return (0);
 }
 
 VCL_STRING
-vmod_hello(const struct vrt_ctx *ctx, VCL_STRING name)
+vmod_hello(VRT_CTX, VCL_STRING name)
 {
 	char *p;
 	unsigned u, v;
